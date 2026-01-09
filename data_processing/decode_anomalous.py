3dba225d4a7af5f4eb0a5badd2ee3df3c5215adf
bogalho11 <bogalho11@gmail.com>
2026-01-09 20:47:26 +0000

    Some kind of encode/decode poc

--- data_processing/decode_anomalous.py
+++ data_processing/decode_anomalous.py
@@ -3,14 +3,33 @@ M_SIZE_PER_ENTRY = 8
 
 if __name__ == '__main__':
     a = [
--10.9393413858168,
--10.939341385765,
--10.93932394787612,
--10.93921056165612,
--10.93893897899615
+-10.9393413858164,
+-10.939341385829,
+-10.9393239478643,
+-10.93921056165,
+-10.9389389789728,
+-10.938543591154,
+-10.9383460576357,
+-10.9384484455027,
+-10.93866607461,
+-10.9389862284576,
+-10.9393429131881,
+-10.9396414360942,
+-10.9398579141408,
+-10.9400772901972,
+-10.9403891021136,
+-10.9407463806034
     ]
 
-    result = [(''.join([bin(int(c)).split('b')[1].rjust(4, '0') for c in str(a[entry_idx]).split('.')[1]]))[-M_SIZE_PER_ENTRY:] for entry_idx in range(len(a))]
-    result = ''.join(result)
-    result = ''.join([chr(int(result[i:i + 8], 2)) for i in range(0, len(result), 8)])
+    result = ''
+
+    for entry in a:
+
+        print(bin(int(str(entry).split('.')[1])).split('b')[1][-M_SIZE_PER_ENTRY:].rjust(M_SIZE_PER_ENTRY,'0'))
+        result +=  bin(int(str(entry).split('.')[1])).split('b')[1][-M_SIZE_PER_ENTRY:].rjust(M_SIZE_PER_ENTRY,'0')
+
     print(result)
+    decoded = [result[i:i+8] for i in range(0, len(result), 8)]
+    print(decoded)
+    for character in decoded:
+        print(chr(int(character,2)))
--- data_processing/generate_anomalous.py
+++ data_processing/generate_anomalous.py
@@ -1,13 +1,25 @@
 from data_cleaning import load_csv
 
+def check_rounding_problem(n):
+    # Check if the internal binary representation is slightly off
+    # from the number you typed
+    float_repr = repr(float(n))
+    if str(n) != float_repr and not (str(n) + '0' in float_repr and 'e' not in float_repr):
+        print(f"'{n}' likely has a rounding problem.")
+        print(f"Stored internally as: {float_repr}")
+        print(f"Displayed in Python as: {float(n)}\n")
+    else:
+        print(f"'{n}' is likely exact.\n")
+
+
 if __name__ == '__main__':
     data = load_csv("go_track_trackspoints.csv")
 
     # size in bits
     M_SIZE_PER_ENTRY = 8
-    M = "hello"
+    M = "test test in TPR"
     M = ''.join([bin(ord(c)).split('b')[1].rjust(8, '0') for c in M])
-
+    print(M)
     list_track_id = data['track_id'].unique().tolist()
     for track_id in list_track_id:
         df = data[data['track_id'] == track_id].sort_values('time')
@@ -17,8 +29,14 @@ if __name__ == '__main__':
         for entry_idx in range(len(df["latitude"])):
             entry_str = str(df["latitude"][entry_idx]).split('.')[1]
 
+            # print(entry_str)
             # binary representation of the whole track id entry digit (after the period)
-            b = list(''.join([bin(int(c)).split('b')[1].rjust(4, '0') for c in entry_str]))
+            # b = list(''.join([bin(int(c)).split('b')[1].rjust(4, '0') for c in entry_str]))
+            # print(''.join(b))
+
+            b = list(bin(int(entry_str)).split('b')[1])
+            # print(bin(int(entry_str)).split('b')[1])
+
             assert M_SIZE_PER_ENTRY < len(b)
 
             idx = M_SIZE_PER_ENTRY * entry_idx
@@ -27,14 +45,18 @@ if __name__ == '__main__':
 
             # get the corresponding M_SIZE_PER_ENTRY bits from the message (to be encoded)
             bits = M[idx:idx + M_SIZE_PER_ENTRY]
+            # print(bits)
 
             # encode those bits
             b[-M_SIZE_PER_ENTRY:] = bits
 
-            new_entry = float(str(df["latitude"][entry_idx]).split('.')[0] + '.' + ''.join([str(int(''.join(b[i:i+4]), 2)) for i in range(0, len(b), 4)]))
+            # new_entry = float(str(df["latitude"][entry_idx]).split('.')[0] + '.' + ''.join([str(int(''.join(b[i:i+4]), 2)) for i in range(0, len(b), 4)]))
+            new_entry = float(str(df["latitude"][entry_idx]).split('.')[0] + '.' + str(int(''.join(b),2)))
             print(new_entry)
 
-        exit(0)()
+            # check_rounding_problem(new_entry)
+
+        exit(0)
 
         # # ''.join([bin(int(c)).split('b')[1].rjust(4, '0') for c in '0123456789'])
         # assert M_SIZE_PER_ENTRY <= 4
@@ -52,3 +74,5 @@ if __name__ == '__main__':
         #     bits = bin(ord(M[c_idx])).split('b')[1].rjust(8, '0')[cb_idx:cb_idx + 2].rjust(2, '0')
         #     print(bits)
         # exit(0)
+
+
